15cb242359f583b7d4b5abb444d7964d81bb5bcf
Mikalai Sokarau <nklskr@gmail.com>
2017-09-28 18:56:23 +0300

    Addes solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  const modules = [0, 0]; //first = module2, second = module5
+
+  return Math.min(...expression.split('*').reduce((acc, item) => {
+    let factorial = parseInt(item),
+        step = item.includes('!!') ? 2 : 1;
+
+    while (factorial > 0) {
+      if (factorial % 2 === 0) {
+        modules[0]++;
+      }
+      if (factorial % 5 === 0) {
+        modules[1]++;
+      }
+      if (factorial % 25 === 0) {
+        modules[1]++;
+      }
+      factorial -= step;
+    }
+
+    return modules;
+  }, modules));
+};
